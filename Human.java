b8744cd201ca420b374a5b5fba3782b2e9d60196
Richard Chen <richardchen39@gmail.com>
2015-05-11 22:27:46 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,23 +1,37 @@
 /**
+ * The Human class represents a human player in the game.
+ * 
  * @author Richard Chen
  *
  */
 public class Human extends Player {
+	/**
+	 * Value indicating a human player
+	 */
 	public static final int HUMAN_PLAYER = -1;
 
+	/**
+	 * The connect-five game
+	 */
 	private Board game;
 
 	/**
+	 * Constructs a human player for the game
 	 * 
 	 * @param game
+	 *            the connect-five game
 	 */
 	public Human(Board game) {
 		this.game = game;
 	}
 
 	/**
+	 * Moves the human player according to the user input
+	 * 
 	 * @param x
+	 *            the x-coordinate of the move
 	 * @param y
+	 *            the y-coordinate of the move
 	 */
 	public void move(int x, int y) {
 		super.move(game, HUMAN_PLAYER, x, y);
